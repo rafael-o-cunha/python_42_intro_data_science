dd0c005306861e309be95cf780df4bd0ca8aa5dd
big <ro.cunha000@gmail.com>
2025-07-05 14:38:04 -0300

    atualizando pasta do projeto e deixando ambiente preparado para começar, ex00 do py 00  finalizado

--- python_00/ex00/Hello.py
+++ python_00/ex00/Hello.py
@@ -1,1 +1,19 @@
-print("Hello World!")
+ft_list = ["Hello", "tata!"]
+ft_tuple = ("Hello", "toto!")
+ft_set = {"Hello", "tutu!"}
+ft_dict = {"Hello" : "titi!"}
+
+ft_list[1] = "World!"
+
+ft_tuple = (ft_tuple[0], "Brazil!")
+
+# para garantir ordem nesse caso seria preciso usar "sorted" na impressão
+ft_set.remove("tutu!")
+ft_set.add("Rio de Janeiro!")
+
+ft_dict["Hello"] = "42Rio!"
+
+print(ft_list)
+print(ft_tuple)
+print(ft_set) 
+print(ft_dict)
