9ebc36ab9865b1392ef7dac66e802e9b3c9f82b9
big <ro.cunha000@gmail.com>
2025-07-16 22:07:33 -0300

    ex05 py00 desenvolvendo

---
+++ python_00/ex05/building.py
@@ -0,0 +1,36 @@
+# ************************************************************************** #
+#                                                                            #
+#                                                        :::      ::::::::   #
+#   building.py                                        :+:      :+:    :+:   #
+#                                                    +:+ +:+         +:+     #
+#   By: rafade-o <rafade-o@student.42.rio>         +#+  +:+       +#+        #
+#                                                +#+#+#+#+#+   +#+           #
+#   Created: 2025/07/13 15:44:23 by rafade-o          #+#    #+#             #
+#   Updated: 2025/07/13 16:00:00 by rafade-o         ###   ########.fr       #
+#                                                                            #
+# ************************************************************************** #
+
+import sys
+from count_itens_inside_data_and_print import count_itens_inside_data_and_print
+
+def main():
+    argc = sys.argv.__len__()
+
+    if argc == 2:
+            data = sys.argv[1]
+    elif argc < 2:
+        while(1):
+            data = input("What is the text to count?")
+            if data != None and data != type(None):
+                break
+    else:
+        print("AssertionError.")
+
+    try:
+        count_itens_inside_data_and_print(data)
+        return
+    except Exception:
+        print("AssertionError: argument is not an integer")
+             
+if __name__ == "__main__":
+    main()
---
+++ python_00/ex05/count_itens_inside_data_and_print.py
@@ -0,0 +1,43 @@
+# ************************************************************************** #
+#                                                                            #
+#                                                        :::      ::::::::   #
+#   count_itens_inside_data_and_print.py.py            :+:      :+:    :+:   #
+#                                                    +:+ +:+         +:+     #
+#   By: rafade-o <rafade-o@student.42.rio>         +#+  +:+       +#+        #
+#                                                +#+#+#+#+#+   +#+           #
+#   Created: 2025/07/05 22:42:11 by rafade-o          #+#    #+#             #
+#   Updated: 2025/07/12 20:47:03 by rafade-o         ###   ########.fr       #
+#                                                                            #
+# ************************************************************************** #
+
+def count_itens_inside_data_and_print(data: str):
+    total_chars = len(data)
+    spaces = data.count(" ")
+    upper_letters = 0
+    lower_letters = 0
+    punctuation_marks = 0
+    digits = 0
+    
+    message = f"""The text contains {total_chars} characters:
+    {upper_letters} upper letters
+    {lower_letters} lower letters
+    {punctuation_marks} punctuation marks
+    {spaces} spaces
+    {digits} digits"""
+
+    i = 0
+    while i < len(total_chars):
+        character = data[i]
+        if character.isdigit() or character.isnumeric() :
+            digits += 1
+        elif character.isalpha() :
+            if character.islower() and character != " ":
+                lower_letters += 1
+            elif character.isupper() and character != " ":
+                upper_letters += 1
+        else:
+            punctuation_marks += 1
+        
+        i += 1
+
+    print(message)
